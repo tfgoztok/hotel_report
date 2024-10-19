d937f61042b045ae2b2932307b42b904204fb5ad
Taner Furkan Göztok <33370154+asilolcu@users.noreply.github.com>
2024-10-19 18:03:55 +0300

    report repo eklendi

---
+++ report-service/src/ReportService/Interfaces/IReportRepository.cs
@@ -0,0 +1,13 @@
+using ReportService.Models;
+
+namespace ReportService.Interfaces
+{
+    public interface IReportRepository
+    {
+        // This interface defines the contract for report repository operations
+        Task<Report> GetByIdAsync(Guid id);
+        Task<IEnumerable<Report>> GetAllAsync();
+        Task AddAsync(Report report);
+        Task UpdateAsync(Report report);
+    }
+}
