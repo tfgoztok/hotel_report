3fdf44d5fcae1b3ae2006f1c39c6b4c2249cd7d9
tfgoztok <furkangoztok09@gmail.com>
2024-10-21 11:52:54 +0300

    hotel-service graphql şeması güncellendi

--- hotel-service/internal/api/graphql/schema.go
+++ hotel-service/internal/api/graphql/schema.go
@@ -7,38 +7,56 @@ import (
 
 // GraphQLService is a struct that holds the hotel service.
 type GraphQLService struct {
-	hotelService *service.HotelService
+	hotelService *service.HotelService // Reference to the hotel service for data retrieval.
 }
 
 // NewGraphQLService initializes a new GraphQLService with the provided hotel service.
 func NewGraphQLService(hotelService *service.HotelService) *GraphQLService {
-	return &GraphQLService{hotelService: hotelService}
+	return &GraphQLService{hotelService: hotelService} // Return a new instance of GraphQLService.
 }
 
 // Schema defines the GraphQL schema for the service, including types and queries.
 func (s *GraphQLService) Schema() (graphql.Schema, error) {
 	// Define the Hotel type with its fields.
 	hotelType := graphql.NewObject(graphql.ObjectConfig{
-		Name: "Hotel",
+		Name: "Hotel", // Name of the GraphQL type.
 		Fields: graphql.Fields{
-			"id":              &graphql.Field{Type: graphql.String},
-			"officialName":    &graphql.Field{Type: graphql.String},
-			"officialSurname": &graphql.Field{Type: graphql.String},
-			"companyTitle":    &graphql.Field{Type: graphql.String},
-			"location":        &graphql.Field{Type: graphql.String},
+			"id":              &graphql.Field{Type: graphql.String}, // Field for hotel ID.
+			"officialName":    &graphql.Field{Type: graphql.String}, // Field for official name.
+			"officialSurname": &graphql.Field{Type: graphql.String}, // Field for official surname.
+			"companyTitle":    &graphql.Field{Type: graphql.String}, // Field for company title.
+			"location":        &graphql.Field{Type: graphql.String}, // Field for location.
+		},
+	})
+
+	// Define the Contact type with its fields.
+	contactType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Contact", // Name of the GraphQL type.
+		Fields: graphql.Fields{
+			"id":      &graphql.Field{Type: graphql.String}, // Field for contact ID.
+			"HotelID": &graphql.Field{Type: graphql.String}, // Field for associated hotel ID.
+			"Type":    &graphql.Field{Type: graphql.String}, // Field for contact type.
+			"Content": &graphql.Field{Type: graphql.String}, // Field for contact content.
 		},
 	})
 
 	// Define the Query type with its fields.
 	queryType := graphql.NewObject(graphql.ObjectConfig{
-		Name: "Query",
+		Name: "Query", // Name of the query type.
 		Fields: graphql.Fields{
 			"hotelsByLocation": &graphql.Field{
-				Type: graphql.NewList(hotelType),
+				Type: graphql.NewList(hotelType), // Return a list of hotels.
 				Args: graphql.FieldConfigArgument{
-					"location": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)},
+					"location": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)}, // Required location argument.
 				},
 				Resolve: s.resolveHotelsByLocation, // Resolver function for this query.
+			},
+			"contactsByLocation": &graphql.Field{
+				Type: graphql.NewList(contactType), // Return a list of contacts.
+				Args: graphql.FieldConfigArgument{
+					"location": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)}, // Required location argument.
+				},
+				Resolve: s.resolveContactsByLocation, // Resolver function for this query.
 			},
 		},
 	})
@@ -54,4 +72,12 @@ func (s *GraphQLService) resolveHotelsByLocation(p graphql.ResolveParams) (inter
 		return nil, nil // Return nil if the location is not valid.
 	}
 	return s.hotelService.GetHotelsByLocation(p.Context, location) // Call the hotel service to get hotels by location.
+}
+
+func (s *GraphQLService) resolveContactsByLocation(p graphql.ResolveParams) (interface{}, error) {
+	location, ok := p.Args["location"].(string) // Extract the location argument.
+	if !ok {
+		return nil, nil // Return nil if the location is not valid.
+	}
+	return s.hotelService.GetContactsByLocation(p.Context, location) // Call the hotel service to get contact by location.
 }
